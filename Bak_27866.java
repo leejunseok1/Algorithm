cc60aadb9eaf5dd81d32a450233eb3e6b7cbb928
leejunseok1 <86453058+leejunseok1@users.noreply.github.com>
2024-06-21 21:29:05 +0900

    Create Bak_27866.java

---
+++ Bak_27866.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Bak_27866 {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
+    int i = sc.nextInt() - 1;
+
+    System.out.println(s.charAt(i));
+    sc.close();
+  }
+}
